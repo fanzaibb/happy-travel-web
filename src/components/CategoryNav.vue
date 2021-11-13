e143730c219669a4d62712c0f236feee9f6e9b2a
fanz <fanzai@heyu.tw>
2021-11-13 12:41:28 +0800

    feat(catgoryNav): basic style

--- src/components/CategoryNav.vue
+++ src/components/CategoryNav.vue
@@ -1,13 +1,46 @@
 <template>
-    <div class="icon-nav flex justify-center">
-        <div v-for="i in 4" :key="i" class="icon-box bg-white flex justify-center items-center">
+    <div class="icon-nav flex justify-center items-center">
+        <div
+            class="toggole-bar w-full absolute flex justify-between"
+            :style="`width: ${getToggleWidth}px;`"
+        >
+            <button class="toggle-btn bg-white rounded-full left-arrow">
+                <div class="toggle-btn-icon">&lt;</div>
+            </button>
+            <button class="toggle-btn bg-white rounded-full left-arrow">
+                <div class="toggle-btn-icon">&gt;</div>
+            </button>
+        </div>
+        <div
+            v-for="item in list"
+            :key="item"
+            class="icon-box bg-white flex justify-center items-center"
+        >
             <div class="h-1/2">
                 <img src="../assets/building.png" alt="hotel" class="m-auto pb-5" />
-                <span class="text-gray-800 text-sm font-medium">在地住宿</span>
+                <span class="text-gray-800 text-sm font-medium">{{ item.name }}</span>
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+
+const cardNum = 4;
+const fullWidth = 183 * cardNum + 20 * (cardNum - 1);
+const getToggleWidth = computed(() => fullWidth + 64);
+
+const list = [
+    { name: '自然風景' },
+    { name: '遊憩類' },
+    { name: '觀光工廠' },
+    { name: '休閒農業' },
+    { name: '生態類' },
+    { name: '古蹟類' },
+    { name: '其他' }
+];
+</script>
 
 <style lang="scss" scoped>
 .icon-nav {
@@ -18,6 +51,17 @@
         border: 1px solid #e0e0e0;
         border-radius: 20px;
         margin: 0 10px;
+    }
+    .toggle-btn {
+        opacity: 0.8;
+        width: 64px;
+        height: 64px;
+        box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.1);
+        .toggle-btn-icon {
+            font-size: 24px;
+            font-weight: 500;
+            @apply text-pink-100;
+        }
     }
 }
 </style>
