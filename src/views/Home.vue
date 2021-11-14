96d8d225b0c7667709412fee94c40cd63585451b
fanz <fanzai@heyu.tw>
2021-11-14 23:32:04 +0800

    feat(home): fetch data & enable all types

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,26 +10,31 @@ const store = useStore();
 
 const cards = [
     { title: '熱門城市', value: 'city', type: 'c' },
-    // { title: '熱門活動', value: 'activity', type: 'h' },
-    { title: '熱門景點', value: 'spot', type: 'v' }
-    // { title: '熱門美食', value: 'restaurant', type: 'v' },
-    // { title: '熱門住宿', value: 'hotel', type: 'v' }
+    { title: '熱門活動', value: 'activity', type: 'h' },
+    { title: '熱門景點', value: 'spot', type: 'v' },
+    { title: '熱門美食', value: 'restaurant', type: 'v' },
+    { title: '熱門住宿', value: 'hotel', type: 'v' }
 ];
-store.dispatch('GET_SPOT');
+
 const data = {
     city: [
-        { id: 'Taipei', name: '台北' },
-        { id: 'Kaohsiung', name: '高雄' },
-        { id: 'Taichung', name: '台中' },
-        { id: 'Tainan', name: '台南' },
-        { id: 'PingtungCounty', name: '屏東' },
-        { id: 'TaitungCounty', name: '台東' }
+        { value: 'Taipei', text: '台北', mask: 'src/assets/taipei-h.png' },
+        { value: 'Kaohsiung', text: '高雄', mask: 'src/assets/tainan-h.png' },
+        { value: 'Taichung', text: '台中', mask: 'src/assets/taichung-h.png' },
+        { value: 'Tainan', text: '台南', mask: 'src/assets/kaohsiung-h.png' },
+        { value: 'PingtungCounty', text: '屏東', mask: 'src/assets/pingtung-h.png' },
+        { value: 'TaitungCounty', text: '台東', mask: '../assets/taitung-h.png' }
     ],
     spot: computed(() => store.state.spot),
-    activity: computed(() => store.state.spot),
+    activity: computed(() => store.state.activity),
     restaurant: computed(() => store.state.restaurant),
     hotel: computed(() => store.state.hotel)
 };
+
+store.dispatch('GET_SPOT');
+store.dispatch('GET_ACTIVITY');
+store.dispatch('GET_RESTAURANT');
+store.dispatch('GET_HOTEL');
 </script>
 
 <template>
