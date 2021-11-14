71b16327c5d191d32fba8b9e8186efc6c2269423
fanz <fanzai@heyu.tw>
2021-11-14 16:54:15 +0800

    feat(header): set link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,42 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import { ref, reactive, computed } from 'vue';
+
+const store = useStore();
+const router = useRouter();
+const infoType = [
+    { name: '景點', value: 'spot' },
+    { name: '美食', value: 'restaurant' },
+    { name: '住宿', value: 'hotel' },
+    { name: '活動', value: 'activity' }
+];
+
+const searchType = type => {
+    store.dispatch('SET_MAIN_TYPE', type);
+};
+
+const backToMain = () => {
+    store.dispatch('UPDATE_NAV');
+    store.dispatch('SET_MAIN_TYPE', '');
+};
+</script>
+
 <template>
-    <header class="fixed top-0 left-0 w-full z-50 bg-white flex items-center">
+    <header class="top-0 left-0 w-full z-50 bg-white flex items-center">
         <nav class="container flex justify-between">
-            <div class="text-4xl font-bold">Taiwan</div>
+            <div class="text-4xl font-bold">
+                <router-link to="/" @click="backToMain">Taiwan</router-link>
+            </div>
             <ul class="flex pt-4">
-                <li class="px-5">景點</li>
-                <li class="px-5">美食</li>
-                <li class="px-5">住宿</li>
-                <li class="pl-5">活動</li>
+                <li
+                    v-for="type in infoType"
+                    :key="type.value"
+                    class="px-5 cursor-pointer"
+                    @click="searchType(type.value)"
+                >
+                    {{ type.name }}
+                </li>
             </ul>
         </nav>
     </header>
