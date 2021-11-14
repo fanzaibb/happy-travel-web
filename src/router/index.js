02fa2a238b826d7876b499c29c7f0d35ee906d9a
fanz <fanzai@heyu.tw>
2021-11-14 16:53:27 +0800

    feat(router): add scroll to top

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
     history: createWebHistory(),
+    scrollBehavior() {
+        // always scroll to top
+        return { top: 0 };
+    },
     routes: [
         {
             path: '/',
