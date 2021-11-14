8f5e2cfe7244e88adbc08ed2662d1585d66619b4
fanz <fanzai@heyu.tw>
2021-11-14 23:28:13 +0800

    feat(search): show text

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -6,6 +6,8 @@ import { reactive, computed } from 'vue';
 
 const store = useStore();
 const searchType = computed(() => store.state.navList.find(e => e.text !== '')?.text);
+const navList = computed(() => store.state.navList);
+console.log(navList.value);
 const data = computed(() => store.state['spot']);
 const list = ['全年無休', '週末開放', '平日開放'];
 const selectList = reactive([
@@ -31,15 +33,44 @@ const addChecked = item => {
     console.log('click');
     item.checked = !item.checked;
 };
+
+const setSearch = () => {
+    const paramObj = {
+        spot: 'ScenicSpot',
+        restaurant: 'Restaurant',
+        hotel: 'Hotel',
+        activity: 'Activity'
+    }
+    const city = navList[0].value;
+    const type = navList[1].value;
+    const others = navList[3].value;
+
+    if (type !== '' ) {
+        const url = `/${paramObj[type]}/${city}?$format=JSON`
+        store.dispatch('GET_SEARCH_DATA');
+    } else if (city !== '') {
+        
+    }
+
+}
 </script>
 
 <template>
     <div class="relative w-full top-bg text-left text-white font-medium flex items-center">
         <section>
             <h2 class="text-lg pb-2">目的地</h2>
-            <h1 class="text-4xl flex">
-                <img src="../assets/w-location.png" alt="" class="mr-3" />台北
-            </h1>
+            <div class="flex">
+                <img
+                    v-if="navList[0].text.length !== 0"
+                    src="../assets/w-location.png"
+                    alt=""
+                    class="mr-3"
+                />
+                <span class="text-4xl">{{ navList[0].text }}</span>
+                <div v-if="navList[2].text !== ''" class="pills px-4 py-2 rounded-full">
+                    #{{ navList[2].text }}
+                </div>
+            </div>
         </section>
     </div>
     <NavTab />
@@ -99,6 +130,9 @@ const addChecked = item => {
     background-image: url('@/assets/city-bg.png');
     background-repeat: no-repeat;
     background-size: cover;
+}
+.pills {
+    background: #ffffff33;
 }
 .text-nav-bar {
     background-color: #ffffff80;
