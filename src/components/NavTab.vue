8ed4780fbaec4780c6a6252d7aefc13b706d072f
fanz <fanzai@heyu.tw>
2021-11-14 16:57:25 +0800

    feat(navTab): conditional search & class

--- src/components/NavTab.vue
+++ src/components/NavTab.vue
@@ -6,8 +6,13 @@ import { computed } from 'vue';
 const store = useStore();
 const router = useRouter();
 const list = computed(() => store.state.navList);
-
 const clickNav = item => {
+    if (item.to === '/') {
+        store.dispatch('UPDATE_NAV');
+        store.dispatch('SET_MAIN_TYPE', '');
+    } else {
+        store.dispatch('UPDATE_NAV', { ...item, clear: true });
+    }
     router.push(item.to);
 };
 </script>
@@ -16,8 +21,14 @@ const clickNav = item => {
     <div class="text-nav-bar text-gray-250 font-nornmal text-sm py-2 text-left">
         <ul class="container flex">
             <li class="cursor-pointer" @click="clickNav({ to: '/' })">首頁</li>
-            <li class="cursor-pointer" v-for="item in list" :key="item.key" @click="clickNav(item)">
-                &nbsp;/ &nbsp;&nbsp;{{ item.label }}
+            <li
+                v-for="(item, index) in list"
+                :key="item.value"
+                v-show="item.value !== ''"
+                :class="{ 'cursor-pointer': index < list.length - 1 }"
+                @click="clickNav(item)"
+            >
+                &nbsp;/ &nbsp;&nbsp;{{ item.text }}
             </li>
         </ul>
     </div>
