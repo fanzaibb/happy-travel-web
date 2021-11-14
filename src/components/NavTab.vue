b7e4401d79f5c5932f3d737d4efc015c5a3fda7e
fanz <fanzai@heyu.tw>
2021-11-14 11:29:27 +0800

    feat(navTab): add component

---
+++ src/components/NavTab.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+
+const store = useStore();
+const router = useRouter();
+const list = computed(() => store.state.navList);
+
+const clickNav = item => {
+    router.push(item.to);
+};
+</script>
+
+<template>
+    <div class="text-nav-bar text-gray-250 font-nornmal text-sm py-2 text-left">
+        <ul class="container flex">
+            <li class="cursor-pointer" @click="clickNav({ to: '/' })">首頁</li>
+            <li class="cursor-pointer" v-for="item in list" :key="item.key" @click="clickNav(item)">
+                &nbsp;/ &nbsp;&nbsp;{{ item.label }}
+            </li>
+        </ul>
+    </div>
+</template>
